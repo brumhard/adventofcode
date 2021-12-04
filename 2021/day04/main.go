541fde55cbfcd4d6058766404facad7b9bf63928
Tobias Brumhard <code@brumhard.com>
2021-12-04 09:32:42 +0100

    Add 2021/day4
    Signed-off-by: Tobias Brumhard <code@brumhard.com>

---
+++ 2021/day04/main.go
@@ -0,0 +1,214 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/brumhard/adventofcode/aocconv"
+)
+
+type Bingo struct {
+	inputs []int
+	boards []Board
+}
+
+type Board struct {
+	fields [][]Field
+	done   bool
+}
+
+func (b *Board) SelectNumber(number int) {
+	for i := range b.fields {
+		for y := range b.fields[i] {
+			if b.fields[i][y].number == number {
+				b.fields[i][y].selected = true
+			}
+		}
+	}
+}
+
+func (b Board) CheckBingo() bool {
+	// this assumes that the board is i x i fields and not i x y
+	for i := range b.fields {
+		if checkVertical(b.fields, i) || checkHorizontal(b.fields, i) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (b Board) SumUnmarked() int {
+	var sum int
+	for _, fieldRow := range b.fields {
+		for _, field := range fieldRow {
+			if field.selected {
+				continue
+			}
+
+			sum += field.number
+		}
+	}
+
+	return sum
+}
+
+func checkVertical(fields [][]Field, index int) bool {
+	for _, fieldRow := range fields {
+		if !fieldRow[index].selected {
+			return false
+		}
+	}
+
+	return true
+}
+
+func checkHorizontal(fields [][]Field, index int) bool {
+	for _, field := range fields[index] {
+		if !field.selected {
+			return false
+		}
+	}
+
+	return true
+}
+
+type Field struct {
+	number   int
+	selected bool
+}
+
+//go:embed input.txt
+var inputFile string
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "an error occured: %s\n", err.Error())
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	input, err := inputFromString(string(inputFile))
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Part 1: %v\n", SolvePart1(input))
+	fmt.Printf("Part 2: %v\n", SolvePart2(input))
+
+	return nil
+}
+
+func inputFromString(inputStr string) (Bingo, error) {
+	inputRows := aocconv.StrToStrSlice(inputStr)
+
+	inputStrs := strings.Split(inputRows[0], ",")
+	inputs := make([]int, 0, len(inputStrs))
+	for _, str := range inputStrs {
+		i, err := strconv.Atoi(str)
+		if err != nil {
+			return Bingo{}, err
+		}
+
+		inputs = append(inputs, i)
+	}
+
+	var (
+		boards        []Board
+		currentFields [][]Field
+		y             int
+	)
+
+	for _, boardRow := range inputRows[2:] {
+		if boardRow == "" && len(currentFields) > 0 {
+			boards = append(boards, Board{fields: currentFields})
+			currentFields = [][]Field{}
+			y = 0
+			continue
+		}
+
+		currentFields = append(currentFields, []Field{})
+		ints := strings.Split(boardRow, " ")
+		for _, fieldStr := range ints {
+			if fieldStr == "" {
+				// additional whitespace in board
+				continue
+			}
+
+			field, err := strconv.Atoi(strings.TrimSpace(fieldStr))
+			if err != nil {
+				return Bingo{}, err
+			}
+
+			currentFields[y] = append(currentFields[y], Field{
+				number: field,
+			})
+		}
+
+		y++
+	}
+	// append last one
+	if len(currentFields) > 0 {
+		boards = append(boards, Board{fields: currentFields})
+	}
+
+	return Bingo{
+		inputs: inputs,
+		boards: boards,
+	}, nil
+}
+
+func SolvePart1(input Bingo) int {
+	winner, finalNumber := findWinner(input)
+
+	if winner.fields == nil {
+		panic("couldn't find winner")
+	}
+
+	return winner.SumUnmarked() * finalNumber
+}
+
+// returns winner with final number
+func findWinner(bingo Bingo) (Board, int) {
+	for _, number := range bingo.inputs {
+		for _, board := range bingo.boards {
+			board.SelectNumber(number)
+			if board.CheckBingo() {
+				return board, number
+			}
+		}
+	}
+
+	return Board{}, 0
+}
+
+func SolvePart2(input Bingo) int {
+	var lastToWin Board
+	var lastNumber int
+	for _, number := range input.inputs {
+		checkedAtLeastOne := false
+		for i := range input.boards {
+			if input.boards[i].done {
+				continue
+			}
+			checkedAtLeastOne = true
+
+			input.boards[i].SelectNumber(number)
+			if input.boards[i].CheckBingo() {
+				input.boards[i].done = true
+				lastToWin = input.boards[i]
+			}
+		}
+		if !checkedAtLeastOne {
+			break
+		}
+
+		lastNumber = number
+	}
+
+	return lastToWin.SumUnmarked() * lastNumber
+}
---
+++ 2021/day04/main_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	inputStr := `7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7
+`
+
+	input, err := inputFromString(inputStr)
+	if err != nil {
+		t.Errorf("failed to load inputs %v", err)
+	}
+
+	t.Run("Test Part1", func(t *testing.T) {
+		got := SolvePart1(input)
+		expected := 4512
+
+		if got != expected {
+			t.Errorf("expected '%d' but got '%d'", expected, got)
+		}
+	})
+
+	t.Run("Test Part2", func(t *testing.T) {
+		got := SolvePart2(input)
+		expected := 1924
+
+		if got != expected {
+			t.Errorf("expected '%d' but got '%d'", expected, got)
+		}
+	})
+}
